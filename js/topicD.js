c9564efe0745521ab5e1d420239f3607ee65ba77
Jamie Pascual <moonshay76@gmail.com>
2026-09-06 18:57:58 -0600

    update api

--- js/topicD.js
+++ js/topicD.js
@@ -15,7 +15,7 @@ btn.addEventListener('click', function () {
     // establish the connection
     rqt.open(
         'GET',
-        'curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" -H "Authorization: Bearer 4e276acf-27ff-',
+        'curl "https://api.open-meteo.com/v1/forecast?latitude=42.3314&longitude=-83.0458&current=temperature_2m,weather_code&temperature_unit=fahrenheit&timezone=auto"',
         true
     );
 
